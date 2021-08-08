7811cb94860fd31be8873f976c0b42ab14a0de4d
iamvahid <vahid.jabarouti@gmail.com>
2021-08-08 11:50:25 +0430

    first commit

---
+++ node-express-jwt-auth-lesson-1/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine 
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://vahid:vahid2357@cluster0.4pk3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => {
+    app.listen(3000)
+    console.log('connected to db...')
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
